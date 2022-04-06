0d67e3067bdfdb5efc2f5192a0a3d1ea78f6fc91
Jessica Nelson <yahwehschica@yahoo.com>
2022-04-06 10:24:48 -0400

    commit tree 3fbc54261ee4f3f32db76f67b9fe7eff9c5dd782

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,65 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
-  return (
+
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+  fetch("http://localhost:4000/questions")
+  .then((res) => res.json())
+  .then((questions) => {
+    setQuestions(questions)
+    })
+   }, [])
+
+   function handleDeleteClick(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      .then(() => {
+        
+        const updatedQuestions = questions.filter((q) => q.id !== id)
+        setQuestions(updatedQuestions)
+      })
+    }
+
+   function handleAnswerChange(id, correctIndex) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ correctIndex }),
+    })
+    .then((res) => res.json())
+    .then((updatedQuestion) => {
+      const updatedQuestions = questions.map((data) => {
+        if (data.id === updatedQuestion.id) 
+        return updatedQuestion
+        return data
+      })
+      setQuestions(updatedQuestions)
+    })
+  }
+
+ 
+
+    const questionItems = questions.map((question) => {
+        return <QuestionItem 
+         key={question.id}
+         question={question}
+         onDeleteClick={handleDeleteClick}
+         answerChange={handleAnswerChange}
+         />
+      })
+    
+
+ return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionItems}</ul>
     </section>
   );
 }
